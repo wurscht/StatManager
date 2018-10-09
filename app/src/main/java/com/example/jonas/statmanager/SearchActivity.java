59c0bcfd479d6d0deb03bc524c04e92c8b6d9692
wurscht <lehjo@gmx.ch>
2018-10-09 23:15:32 +0200

    Add redirect button to MainActivity

--- app/src/main/java/com/example/jonas/statmanager/SearchActivity.java
+++ app/src/main/java/com/example/jonas/statmanager/SearchActivity.java
@@ -1,22 +1,35 @@
 package com.example.jonas.statmanager;
 
-import android.app.SearchManager;
-import android.content.Intent;
+// import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+// import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
 
 public class SearchActivity extends AppCompatActivity {
+    Button searchBtn;
+    SearchView searchField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.search);
+        setContentView(R.layout.activity_detail);
 
-        Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            intent.putExtra("username", query);
-            startActivity(intent);
-        }
+        searchBtn = (Button) findViewById(R.id.search_button);
+        searchField = (SearchView) findViewById(R.id.search_field);
+
+
+        
+        //searchBtn.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+        //        String input = searchField.getQuery().toString();
+        //        intent.putExtra("username", input);
+        //        startActivity(intent);
+
+        //    }
+        //});
     }
 }

7a6ed1b7bd9812cd5bb87f5913d66094cf71f41b
Jonas Lehmann <jonas.lehmann@adfinis-sygroup.ch>
2018-10-10 17:34:32 +0200

    Fix problem with redirect button

--- app/src/main/java/com/example/jonas/statmanager/SearchActivity.java
+++ app/src/main/java/com/example/jonas/statmanager/SearchActivity.java
@@ -1,9 +1,9 @@
 package com.example.jonas.statmanager;
 
-// import android.content.Intent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-// import android.view.View;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
 
@@ -14,22 +14,22 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_search);
 
         searchBtn = (Button) findViewById(R.id.search_button);
         searchField = (SearchView) findViewById(R.id.search_field);
 
 
         
-        //searchBtn.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View v) {
-        //        Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
-        //        String input = searchField.getQuery().toString();
-        //        intent.putExtra("username", input);
-        //        startActivity(intent);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                String input = searchField.getQuery().toString();
+                intent.putExtra("username", input);
+                startActivity(intent);
 
-        //    }
-        //});
+            }
+        });
     }
 }
